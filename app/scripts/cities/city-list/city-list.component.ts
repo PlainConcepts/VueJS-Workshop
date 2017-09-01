98b72547fca7e65f65076e0aac9e0dc1d2d200f3
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-09-01 19:21:18 +0200

    Clean add

--- app/scripts/cities/city-list/city-list.component.ts
+++ app/scripts/cities/city-list/city-list.component.ts
@@ -1,17 +1,14 @@
 import { Component, BaseComponent } from '~/core';
 import { City, citiesService } from '~/shared';
-import { CityAddComponent } from '../shared';
 
 import Template from './city-list.component.html?style=cities/city-list/city-list.component.css';
 
 @Template
-@Component({
-    components: {
-        [CityAddComponent.tag]: CityAddComponent
-    }
-})
+@Component
 export class CityListComponent extends BaseComponent {
     public cities: City[] = null;
+    public city: City = null;
+    public name = '';
 
     public async created() {
         this.cities = await citiesService.get();
@@ -19,5 +16,13 @@ export class CityListComponent extends BaseComponent {
 
     public remove(id: number) {
         citiesService.remove(id);
+    }
+
+    public async search() {
+        if (!this.name) {
+            return;
+        }
+        
+        this.city = await citiesService.search(this.name);
     }
 }
